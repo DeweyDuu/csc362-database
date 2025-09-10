a979e544d6f1a39fef65b68f86007c830863dd07
DeweyDu <weicheng.du@centre.edu>
2025-09-10 13:53:12 +0000

    final version

--- labs/lab01_2b.sql
+++ labs/lab01_2b.sql
@@ -1,5 +1,23 @@
-DROP DATABASE IF EXISTS bookstore;
+DROP DATABASE IF EXISTS school;
 
-CREATE DATABASE bookstore;
+CREATE DATABASE school;
+
+USE school;
+
+CREATE TABLE instructors(
+	PRIMARY KEY (inst_id),
+	inst_id INT,
+	campus_phone CHAR(8),
+	inst_first_name	VARCHAR(20),
+	inst_last_name	VARCHAR(20)
+);
+INSERT INTO instructors (inst_id,inst_first_name, inst_last_name, campus_phone)
+VALUES ('1','Kira', 'Bently', '363-9948'),
+	('2','Timothy','Ennis','527-4992'),
+	('3','Shannon','Black','322-5992'),
+	('4','Estela','Rosales','322-6992');
+
+	
+SELECT * FROM instructors;
 
 
